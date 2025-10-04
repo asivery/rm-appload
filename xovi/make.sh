33ba73c9052d15eacdc0f5bce1e6bfbdbb695d27
ingatellent <github@spam.ingatellent.dk>
2025-10-04 16:05:31 +0200

    Update make.sh
    To work with newest toolchain version of rcc

--- xovi/make.sh
+++ xovi/make.sh
@@ -3,7 +3,7 @@ rm -rf temporary
 mkdir -p temporary
 
 cp -rv ../src temporary
-rcc --no-compress -g cpp ../resources/resources.qrc | sed -n '/#ifdef/q;p' > temporary/resources.cpp
+rcc --no-compress -g cpp ../resources/resources.qrc | sed '/#ifdef _MSC_VER/,/#endif/d' | sed -n '/#ifdef/q;p' > temporary/resources.cpp
 cp main.cpp config.h temporary/src/
 
 qmake6 .
