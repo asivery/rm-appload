9a6a6e2008ee821b8ba5e56f4b6b2b5f9f81f25c
Florian <29229062+Raupinger@users.noreply.github.com>
2026-05-22 03:42:40 +0200

    fix rust qtfb example (#51)

--- examples/qtfb/rust/src/main.rs
+++ examples/qtfb/rust/src/main.rs
@@ -1,13 +1,15 @@
 use qtfb_client::ClientConnection;
+use std::env;
 
 fn main() {
+    let key: u32 =  env::var("QTFB_KEY").expect("couldnt find QTFB_KEY environment variable. Are you sure you launched from appload?").parse::<u32>().expect("qtfb not a number");
     let client = ClientConnection::new(
-        qtfb_client::constants::DEFAULT_SCENE,
+        key,
         qtfb_client::constants::FBFMT_RMPP_RGB888,
         None
     ).unwrap();
     let file_contents = std::fs::read("a.raw").unwrap();
     client.shm[0..file_contents.len()].copy_from_slice(&file_contents);
-    client.send_complete_update().unwrap();
+    let res = client.send_complete_update().unwrap();
     std::thread::sleep(std::time::Duration::from_secs(10));
 }
